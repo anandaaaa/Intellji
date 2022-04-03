5d09391eb5bf322e7e1aafc8e61e15fc75fb5a24
anandwi <102906693+anandwi@users.noreply.github.com>
2022-04-03 17:58:28 +0530

    Update devclass.java

--- src/main/java/devclass.java
+++ src/main/java/devclass.java
@@ -3,5 +3,6 @@ public class devclass {
     public static  void main(String args)
     {
         System.out.println("hello world");
+        //this is the stream of changes + commiting in stream 2 learn
     }
 }
